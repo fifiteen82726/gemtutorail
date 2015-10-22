79af49ebc6773b00b1356aec755625229edd805f
fifiteen82726 <fifiteen82726@gmail.com>
2015-10-22 10:59:07 +0800

    list ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,15 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
+
+(function() {
+  $(function() {
+    return $('[data-toggle]').on('click', function() {
+      var toggle;
+      toggle = $(this).addClass('active').attr('data-toggle');
+      $(this).siblings('[data-toggle]').removeClass('active');
+      return $('.surveys').removeClass('grid list').addClass(toggle);
+    });
+  });
+
+}).call(this);
